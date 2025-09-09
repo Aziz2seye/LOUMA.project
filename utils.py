1a07dfa3cc25702f3407da3eab776974e6ebb9f6
souleyman03 <s.anne@cat.edu.sn>
2025-09-09 13:18:01 +0200

    Yallah bakhna

--- utils.py
+++ utils.py
@@ -4,6 +4,16 @@ import pandas as pd
 DATA_PATH = "vto_list.xlsx"
 def load_vto():
     try:
-        return pd.read_excel(DATA_PATH)
+        return pd.read_excel(DATA_PATH, sheet_name="vto")
     except FileNotFoundError:
         return pd.DataFrame(columns=["LOGIN", "PRENOM", "NOM"])
+    
+
+def load_pvt():
+    try:
+        return pd.read_excel(DATA_PATH, sheet_name="pvt")
+    except FileNotFoundError:
+        return pd.DataFrame(columns=["DRV", "PVT", "CONTACT"])
+
+
+
